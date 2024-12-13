9c830e01d01d8927d60534cbca532c16f58d36a2
polurumanasa3 <manasap142@gmail.com>
2024-12-12 20:55:42 -0800

    Add files via upload

---
+++ a.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class a
+{
+    public static void main(String args[])
+    {
+        LinkedList<String> a=new LinkedList<>();
+        a.add("Hello");
+        a.add("Iam");
+        a.add("students");
+        System.out.println(a);
+    }
+}
---
+++ b.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class b
+{
+    public static void main(String args[])
+    {
+        LinkedList<String> a=new LinkedList<>();
+        a.add("Hello");
+        a.add("Iam");
+        a.add("students");
+        System.out.println(a);
+        a.remove(0);
+        System.out.println(a);
+        a.clear();
+        System.out.println(a);
+    }
+}
+    
